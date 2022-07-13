1d527b5501571783df85cbf486218dccb69ebb3e
carloslevo <madrilevo16@gmail.com>
2022-07-13 18:44:57 +0200

    gasreceipt

---
+++ GasReceipt.java
@@ -0,0 +1,20 @@
+package day02_PrintingComments;
+
+public class GasReceipt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("---------------------------------------");
+		System.out.println("-           MCLEAN Store              -");
+		System.out.println("-  2019-06-19           04:38PM       -");
+		System.out.println("-  GALLONS 10.870                     -");
+		System.out.println("-  PRICE/Gallon : $2.89               -");
+		System.out.println("-  Fuel Total:    $22.71              -");
+		System.out.println("---------------------------------------");
+		
+		double a =5.2;
+		double b = Math.ceil(a);
+		System.out.println(a+" "+b);
+	}
+
+}
